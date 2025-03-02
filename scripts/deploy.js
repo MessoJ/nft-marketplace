2d8c1e745e413a91c0037ca052a275c217ac84dc
Meso Francis <118773373+MessoJ@users.noreply.github.com>
2025-03-02 23:18:41 +0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,24 +3,29 @@ const hre = require("hardhat");
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
   console.log("Deploying contracts with account:", deployer.address);
-
+  
+  // Deploy first marketplace contract (if you need both)
+  const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+  const nftMarketplace = await NFTMarketplace.deploy();
+  await nftMarketplace.deployed();
+  console.log("NFTMarketplace deployed to:", nftMarketplace.address);
+  
   // Deploy Token
   const Token = await hre.ethers.getContractFactory("MyToken");
   const token = await Token.deploy();
   await token.deployed();
-
+  console.log("Token address:", token.address);
+  
   // Deploy NFT
   const NFT = await hre.ethers.getContractFactory("MyNFT");
   const nft = await NFT.deploy();
   await nft.deployed();
-
+  console.log("NFT address:", nft.address);
+  
   // Deploy Marketplace
   const Marketplace = await hre.ethers.getContractFactory("Marketplace");
   const marketplace = await Marketplace.deploy(nft.address, token.address);
   await marketplace.deployed();
-
-  console.log("Token address:", token.address);
-  console.log("NFT address:", nft.address);
   console.log("Marketplace address:", marketplace.address);
 }
 
